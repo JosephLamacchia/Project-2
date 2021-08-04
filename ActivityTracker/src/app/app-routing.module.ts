f887dcc41e12a32b256b9e227f5e15de9b98c15a
Christopher Nowalk <christopher.nowalk@snhu.edu>
2021-08-04 11:16:35 -0400

    Toggle diplay service added

--- ActivityTracker/src/app/app-routing.module.ts
+++ ActivityTracker/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewUserRegistrationComponent } from './components/new-user-registration/new-user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'newUser', component: NewUserRegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
