d4a393afd66115c46a5155dd17eee0d94f09eea0
Christopher Nowalk <christopher.nowalk@snhu.edu>
2021-08-05 12:20:32 -0400

    Login page moved to new component, switing between components now work

--- ActivityTracker/src/app/app-routing.module.ts
+++ ActivityTracker/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 import { NewUserRegistrationComponent } from './components/new-user-registration/new-user-registration.component';
 
 const routes: Routes = [
-  {path: 'newUser', component: NewUserRegistrationComponent}
+  {path: 'login', component: LoginPageComponent},
+  {path: 'newUser', component: NewUserRegistrationComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
 @NgModule({
---
+++ ActivityTracker/src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ToggleDisplayService } from 'src/app/services/toggle-display.service';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent{
+
+  constructor() {
+
+   }
+
+  ngOnInit(): void {
+
+  }
+
+}
