abb68f4b9cf39cba5334badbc0dce4edc1b8175c
Christopher Nowalk <christopher.nowalk@snhu.edu>
2021-08-06 17:16:17 -0400

    Injected http service

--- ActivityTracker/src/app/components/new-user-registration/new-user-registration.component.ts
+++ ActivityTracker/src/app/components/new-user-registration/new-user-registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Employee } from 'src/models/Employee';
+import { EmployeeHttpService } from 'src/services/employee_service/employee-http.service';
 
 //need to inject the Http service and the user service into this component
 @Component({
@@ -10,17 +11,17 @@ import { Employee } from 'src/models/Employee';
 })
 export class NewUserRegistrationComponent{
 
-  //this is just a list of fake managers, I will create a real list once we have all of the 
-  //back end capabilities and database 
+  //I need to use the get all employees and filter our the managers to populate this list
   managers :string[] = ['Chris', 'Jugue', 'Joseph', 'Nicolis', 'Dwayne'];
 
-  firstName :string | undefined;
-  lastName :string | undefined;
-  email :string | undefined;
-  password :string | undefined;
+  firstName :string="";
+  lastName :string="";
+  email :string="";
+  password :string="";
   isManager :boolean = false;
+  m_id :number | undefined;
 
-  constructor(public fb :FormBuilder) { }
+  constructor(public fb :FormBuilder, public eHttpService: EmployeeHttpService) { }
 
   managersForm = this.fb.group({
     name :['']
@@ -45,6 +46,15 @@ export class NewUserRegistrationComponent{
     console.log(this.email);
     console.log(this.password);
     console.log(this.isManager);
+
+    let employee = new Employee(0, this.email, this.password, this.firstName, this.lastName, this.isManager, 0);
+    console.log(employee);
+
+    this.eHttpService.addEmployee(employee).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    )
   }
 
 }
