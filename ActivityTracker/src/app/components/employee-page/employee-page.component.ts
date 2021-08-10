217439bf2ba1da347df3c25b31262c703036eafc
Christopher Nowalk <christopher.nowalk@snhu.edu>
2021-08-10 16:12:18 -0400

    Added list of tasks per employee, saved those tasks in localstorage

--- ActivityTracker/src/app/components/employee-page/employee-page.component.ts
+++ ActivityTracker/src/app/components/employee-page/employee-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TaskHttpService } from 'src/services/task_service/task-http.service';
+import { LoginmatchService } from 'src/services/loginmatch.service';
+import { Login } from 'src/models/Login';
+import { Task } from 'src/models/Task';
+import { stringify } from '@angular/compiler/src/util';
 
 
 @Component({
@@ -9,18 +14,53 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class EmployeePageComponent implements OnInit {
+ //we need to make sure after we log in we can still retrieve the current employee
+ tasks :Task[] = [];
+ curTasks :Task[] = [];
+ currentEmployee :Login | undefined;
 
-  constructor(private http: HttpClient) { }
+ constructor(private http: HttpClient, private ts: TaskHttpService, private ls: LoginmatchService) { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+   this.getTasks();
+ }
 
-  onSubmit(data: any)
-  {
-    this.http.post('http://localhost:8080/task', data)
-    .subscribe((result) =>{
-      console.warn("result", result)
-    })
-    console.warn(data);
-  }
+ // onSubmit(data: any)
+ // {
+ //   this.http.post('http://localhost:8080/task', data)
+ //   .subscribe((result) =>{
+ //     console.warn("result", result)
+ //   })
+ //   console.warn(data);
+ // }
+ getTasks() {
+       //get the currently logged in employee
+       this.currentEmployee = this.ls.currentLogin;
+       console.log(this.currentEmployee);
+       //on init I will load all of the tasks this employee currently has
+       //call the get all tasks method in the task services
+       this.ts.getAllTasks().subscribe(
+         (reponse) => {
+           //okay we can properly get all of the tasks
+           //console.log(reponse);
+           this.tasks = reponse;
+           console.log(this.tasks);
+           //now we need to sort through them and take out any which don't apply to the current employee
+           //I didn't know how to remove things from an array easily in typescript, so I just added them to another array.
+           for(let i = 0; i < this.tasks.length; i++) {
+             if(this.tasks[i].e_id == this.currentEmployee?.id) {
+               this.curTasks.push(this.tasks[i]);
+               let lsObject = stringify(this.tasks[i]);
+               localStorage.setItem("task " + (i + 1), lsObject);
+             }
+           }
+           console.log(this.curTasks);
+         }
+       )
+       //filter out all of the tasks not associated to this employee
+ }
+
+ updateTask() {
+   //here is where we have the functionality of updating the task
+ }
 }
