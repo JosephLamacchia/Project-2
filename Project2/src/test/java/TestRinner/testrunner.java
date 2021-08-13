1e0b1bc148f6c6d39dad16016dcfa29adc681a45
dwayne-revature <85714703+dwayne-revature@users.noreply.github.com>
2021-08-12 21:26:28 -0400

    Add files via upload

---
+++ Project2/src/test/java/TestRinner/testrunner.java
@@ -0,0 +1,12 @@
+package TestRinner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\josep\\git\\Project-2\\Project2\\src\\test\\java\\com\\revature\\features")
+public class testrunner {
+
+}
